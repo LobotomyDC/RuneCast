2aca5cb8be80544b6a608619a457abae48d992f6
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2025-01-26 05:49:42 -0500

    Fix mudclient-sdl12.c
    Missing special symbols on keyboard

--- src/mudclient-sdl12.c
+++ src/mudclient-sdl12.c
@@ -17,7 +17,7 @@
 // D-Pad movement speed (pixels per frame)
 #define DPAD_SPEED 5
 
-// TODO: See if this can be handled differently
+// We keep these static variables for tracking mouse position
 static int s_cursor_x = FRAMEBUFFER_WIDTH / 2;
 static int s_cursor_y = FRAMEBUFFER_HEIGHT / 2;
 
@@ -89,7 +89,7 @@ void mudclient_poll_events(mudclient *mud) {
                 break;
 
             case SDL_JOYAXISMOTION: {
-                // Joystick movements should also update the same cursor position
+                // Joystick movements also update the same cursor
                 if (event.jaxis.axis == 0) { // Left Stick X-axis
                     s_cursor_x += (int)((event.jaxis.value / 32767.0f) * DPAD_SPEED);
                 } else if (event.jaxis.axis == 1) { // Left Stick Y-axis
@@ -123,7 +123,7 @@ void mudclient_poll_events(mudclient *mud) {
         } // end switch
     } // end while (SDL_PollEvent)
 
-    // Final boundary check after all events
+    // Final boundary check after all events TODO: See if we even need this
     if (s_cursor_x < 0) s_cursor_x = 0;
     if (s_cursor_x >= FRAMEBUFFER_WIDTH)  s_cursor_x = FRAMEBUFFER_WIDTH - 1;
     if (s_cursor_y < 0) s_cursor_y = 0;
@@ -186,14 +186,14 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
     *code = -1;
     *char_code = -1;
 
-    if (keysym->unicode > 0 && keysym->unicode < 128) {
-        if (isprint((unsigned char)keysym->unicode)) {
-            *code = keysym->unicode;
-            *char_code = keysym->unicode;
-            return;
-        }
+    // Check for printable characters directly
+    if (keysym->unicode > 0 && keysym->unicode < 128 && isprint((unsigned char)keysym->unicode)) {
+        *code = keysym->unicode;
+        *char_code = keysym->unicode;
+        return;
     }
 
+    // Handle specific keys
     switch (keysym->sym) {
         case SDLK_TAB: 
             *code = K_TAB; 
@@ -203,90 +203,123 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
             *code = K_BACKSPACE; 
             *char_code = '\b'; 
             break;
-        case SDLK_LEFT: 
-            *code = K_LEFT; 
-            break;
-        case SDLK_RIGHT: 
-            *code = K_RIGHT; 
-            break;
-        case SDLK_UP: 
-            *code = K_UP; 
-            break;
-        case SDLK_DOWN: 
-            *code = K_DOWN; 
-            break;
-        case SDLK_PAGEUP: 
-            *code = K_PAGE_UP; 
-            break;
-        case SDLK_PAGEDOWN: 
-            *code = K_PAGE_DOWN; 
-            break;
-        case SDLK_HOME: 
-            *code = K_HOME; 
-            break;
-        case SDLK_F1: case SDLK_F2: case SDLK_F3: case SDLK_F4: case SDLK_F5:
-        case SDLK_F6: case SDLK_F7: case SDLK_F8: case SDLK_F9:
-        case SDLK_F10: case SDLK_F11: case SDLK_F12:
-            *code = keysym->sym; 
-            break; // Function keys
-        case SDLK_ESCAPE: 
-            *code = K_ESCAPE; 
-            break;
         case SDLK_RETURN: 
             *code = K_ENTER; 
             *char_code = '\r'; 
             break;
-        case SDLK_SPACE: 
-            *code = ' '; 
-            *char_code = ' '; 
-            break;
-        case SDLK_COMMA: 
-            *code = ','; 
-            *char_code = ','; 
-            break;
-        case SDLK_PERIOD: 
-            *code = '.'; 
-            *char_code = '.'; 
-            break;
-        case SDLK_QUOTE: 
-            *code = '\''; 
-            *char_code = '\''; 
-            break;
-        case SDLK_BACKQUOTE: 
-            *code = '`'; 
-            *char_code = '`'; 
-            break;
-        case SDLK_CAPSLOCK: 
-            *code = KMOD_CAPS; 
-            break;
-        case SDLK_LSHIFT: case SDLK_RSHIFT: 
-            *code = KMOD_SHIFT; 
+        case SDLK_ESCAPE: 
+            *code = K_ESCAPE; 
             break;
 
+        // Arrow keys
+        case SDLK_LEFT: case SDLK_RIGHT: case SDLK_UP: case SDLK_DOWN:
+            *code = keysym->sym;
+            break;
+
+        // Function keys
+        case SDLK_F1: case SDLK_F2: case SDLK_F3: case SDLK_F4: case SDLK_F5:
+        case SDLK_F6: case SDLK_F7: case SDLK_F8: case SDLK_F9:
+        case SDLK_F10: case SDLK_F11: case SDLK_F12:
+            *code = keysym->sym;
+            break;
+
+        // Alphabet (upper-case and lower-case)
         case SDLK_a: case SDLK_b: case SDLK_c: case SDLK_d: case SDLK_e:
         case SDLK_f: case SDLK_g: case SDLK_h: case SDLK_i: case SDLK_j:
         case SDLK_k: case SDLK_l: case SDLK_m: case SDLK_n: case SDLK_o:
         case SDLK_p: case SDLK_q: case SDLK_r: case SDLK_s: case SDLK_t:
         case SDLK_u: case SDLK_v: case SDLK_w: case SDLK_x: case SDLK_y:
         case SDLK_z:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? toupper(keysym->sym) : tolower(keysym->sym);
             *code = keysym->sym;
-            *char_code = keysym->sym; // Handles lowercase letters
             break;
 
-        case SDLK_0: case SDLK_1: case SDLK_2: case SDLK_3: case SDLK_4:
-        case SDLK_5: case SDLK_6: case SDLK_7: case SDLK_8: case SDLK_9:
+        // Numeric and shifted symbols
+        case SDLK_1:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '!' : '1';
             *code = keysym->sym;
-            *char_code = keysym->sym; // Numeric characters
+            break;
+        case SDLK_2:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '@' : '2';
+            *code = keysym->sym;
+            break;
+        case SDLK_3:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '#' : '3';
+            *code = keysym->sym;
+            break;
+        case SDLK_4:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '$' : '4';
+            *code = keysym->sym;
+            break;
+        case SDLK_5:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '%' : '5';
+            *code = keysym->sym;
+            break;
+        case SDLK_6:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '^' : '6';
+            *code = keysym->sym;
+            break;
+        case SDLK_7:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '&' : '7';
+            *code = keysym->sym;
+            break;
+        case SDLK_8:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '*' : '8';
+            *code = keysym->sym;
+            break;
+        case SDLK_9:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '(' : '9';
+            *code = keysym->sym;
+            break;
+        case SDLK_0:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? ')' : '0';
+            *code = keysym->sym;
             break;
 
-        // Numpad Keys
-        case SDLK_KP0: case SDLK_KP1: case SDLK_KP2: case SDLK_KP3:
-        case SDLK_KP4: case SDLK_KP5: case SDLK_KP6: case SDLK_KP7:
-        case SDLK_KP8: case SDLK_KP9: case SDLK_KP_PERIOD:
-        case SDLK_KP_DIVIDE: case SDLK_KP_MULTIPLY: case SDLK_KP_MINUS:
-        case SDLK_KP_PLUS: case SDLK_KP_ENTER: case SDLK_KP_EQUALS:
+        // Punctuation and symbols
+        case SDLK_MINUS:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '_' : '-';
             *code = keysym->sym;
-            *char_code = keysym->unicode; // Handles numpad characters
+            break;
+        case SDLK_EQUALS:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '+' : '=';
+            *code = keysym->sym;
+            break;
+        case SDLK_BACKQUOTE:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '~' : '`';
+            *code = keysym->sym;
+            break;
+        case SDLK_LEFTBRACKET:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '{' : '[';
+            *code = keysym->sym;
+            break;
+        case SDLK_RIGHTBRACKET:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '}' : ']';
+            *code = keysym->sym;
+            break;
+        case SDLK_BACKSLASH:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '|' : '\\';
+            *code = keysym->sym;
+            break;
+        case SDLK_SEMICOLON:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? ':' : ';';
+            *code = keysym->sym;
+            break;
+        case SDLK_QUOTE:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '"' : '\'';
+            *code = keysym->sym;
+            break;
+        case SDLK_COMMA:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '<' : ',';
+            *code = keysym->sym;
+            break;
+        case SDLK_PERIOD:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '>' : '.';
+            *code = keysym->sym;
+            break;
+        case SDLK_SLASH:
+            *char_code = (keysym->mod & KMOD_SHIFT) ? '?' : '/';
+            *code = keysym->sym;
             break;
 
         default:
