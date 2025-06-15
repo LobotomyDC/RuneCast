48a28b63b2ba749a6ad8d507227e873d66c21e7f
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2025-06-15 18:08:39 -0400

    Delete SDL2-dreamcastSDL2 directory

--- SDL2-dreamcastSDL2/test/dreamcast/test/sound/dc.sh
+++
@@ -1,5 +0,0 @@
-sh-elf-objcopy -R .stack -O binary SDL2dcSoundTest.elf SDL2dcSoundTest.bin
-$KOS_BASE/utils/scramble/scramble SDL2dcSoundTest.bin 1st_read.bin
-cp 1st_read.bin cd/
-~/code/dreamcast/mkdcdisc/builddir/mkdcdisc -n DCVice -N -d cd -d cd/data -e SDL2dcSoundTest.elf -o SDL2dcSoundTest.cdi -I -N
-
