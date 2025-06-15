158b665059cac6631477eef3543582db4b3f8717
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2025-06-15 18:06:20 -0400

    Update mudclient-sdl2.c

--- src/mudclient-sdl2.c
+++ src/mudclient-sdl2.c
@@ -1,73 +1,76 @@
 #include "mudclient.h"
+#include <kos/dbglog.h>
 
 #ifdef SDL2
 void get_sdl_keycodes(SDL_Keysym *keysym, char *char_code, int *code) {
     *code = -1;
     *char_code = -1;
 
+    SDL_Keymod mod = keysym->mod;
+
+    SDL_Keycode sym = keysym->sym;
+
+    // First handle special keys
     switch (keysym->scancode) {
-    case SDL_SCANCODE_LEFT:
-        *code = K_LEFT;
-        break;
-    case SDL_SCANCODE_RIGHT:
-        *code = K_RIGHT;
-        break;
-    case SDL_SCANCODE_UP:
-        *code = K_UP;
-        break;
-    case SDL_SCANCODE_DOWN:
-        *code = K_DOWN;
-        break;
-    case SDL_SCANCODE_PAGEUP:
-        *code = K_PAGE_UP;
-        break;
-    case SDL_SCANCODE_PAGEDOWN:
-        *code = K_PAGE_DOWN;
-        break;
-    case SDL_SCANCODE_HOME:
-        *code = K_HOME;
-        break;
-    case SDL_SCANCODE_F1:
-        *code = K_F1;
-        break;
-    case SDL_SCANCODE_ESCAPE:
-        *code = K_ESCAPE;
-        break;
-    case SDL_SCANCODE_KP_ENTER:
-    case SDL_SCANCODE_RETURN:
-        *code = K_ENTER;
-        *char_code = K_ENTER;
-        break;
-    // TODO: Swallow "bad inputs" by default? ie. numlock, capslock
-    case SDL_SCANCODE_NUMLOCKCLEAR:
-        *code = -1;
-        *char_code = 1;
-        break;
-    case SDL_SCANCODE_CAPSLOCK:
-        *code = -1;
-        *char_code = 1;
-        break;
-    case SDL_SCANCODE_TAB:
-        *code = K_TAB;
-        *char_code = K_TAB;
-        break;
-    case SDL_SCANCODE_BACKSPACE:
-        *code = K_BACKSPACE;
-        *char_code = K_BACKSPACE;
-        break;
-    default:
-        break;
+        case SDL_SCANCODE_LEFT:  *code = K_LEFT; break;
+        case SDL_SCANCODE_RIGHT: *code = K_RIGHT; break;
+        case SDL_SCANCODE_UP:    *code = K_UP; break;
+        case SDL_SCANCODE_DOWN:  *code = K_DOWN; break;
+        case SDL_SCANCODE_PAGEUP:   *code = K_PAGE_UP; break;
+        case SDL_SCANCODE_PAGEDOWN: *code = K_PAGE_DOWN; break;
+        case SDL_SCANCODE_HOME:     *code = K_HOME; break;
+        case SDL_SCANCODE_ESCAPE:   *code = K_ESCAPE; break;
+        case SDL_SCANCODE_F1:       *code = K_F1; break;
+        case SDL_SCANCODE_RETURN:
+        case SDL_SCANCODE_KP_ENTER: *code = K_ENTER; *char_code = K_ENTER; break;
+        case SDL_SCANCODE_TAB:      *code = K_TAB;   *char_code = K_TAB; break;
+        case SDL_SCANCODE_BACKSPACE:*code = K_BACKSPACE; *char_code = K_BACKSPACE; break;
+        default: break;
+    }
+
+    // Handle ASCII keys (letters, numbers, symbols)
+    if (sym >= 32 && sym <= 126) {
+        if ((mod & KMOD_SHIFT)) {
+            // Shift-modified output
+            if (sym >= 'a' && sym <= 'z') {
+                *char_code = sym - 32; // lowercase → uppercase
+            } else {
+                // Shift-symbol mapping for common characters
+                switch (sym) {
+                    case '1': *char_code = '!'; break;
+                    case '2': *char_code = '@'; break;
+                    case '3': *char_code = '#'; break;
+                    case '4': *char_code = '$'; break;
+                    case '5': *char_code = '%'; break;
+                    case '6': *char_code = '^'; break;
+                    case '7': *char_code = '&'; break;
+                    case '8': *char_code = '*'; break;
+                    case '9': *char_code = '('; break;
+                    case '0': *char_code = ')'; break;
+                    case '`': *char_code = '~'; break;
+                    case '-': *char_code = '_'; break;
+                    case '=': *char_code = '+'; break;
+                    case '[': *char_code = '{'; break;
+                    case ']': *char_code = '}'; break;
+                    case '\\':*char_code = '|'; break;
+                    case ';': *char_code = ':'; break;
+                    case '\'':*char_code = '"'; break;
+                    case ',': *char_code = '<'; break;
+                    case '.': *char_code = '>'; break;
+                    case '/': *char_code = '?'; break;
+                    default: *char_code = sym; break;
+                }
+            }
+        } else {
+            *char_code = sym;
+        }
     }
 }
 
 void mudclient_start_application(mudclient *mud, char *title) {
-#ifdef __SWITCH__
-    Result romfs_res = romfsInit();
-
-    if (romfs_res) {
-        mud_error("romfsInit: %08lX\n", romfs_res);
-        exit(1);
-    }
+#ifndef RENDER_GL
+    // Prevent SDL2 from selecting OpenGL render backend unless explicitly requested
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "Dreamcast PVR");
 #endif
 
     int init = SDL_INIT_VIDEO;
@@ -76,10 +79,6 @@ void mudclient_start_application(mudclient *mud, char *title) {
         init |= SDL_INIT_AUDIO;
     }
 
-#ifdef __SWITCH__
-    init |= SDL_INIT_JOYSTICK;
-#endif
-
     if (SDL_Init(init) < 0) {
         mud_error("SDL_Init(): %s\n", SDL_GetError());
         exit(1);
@@ -87,11 +86,6 @@ void mudclient_start_application(mudclient *mud, char *title) {
 
 #ifdef SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH
     SDL_SetHint(SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
-#endif
-
-#ifdef __SWITCH__
-    SDL_JoystickEventState(SDL_ENABLE);
-    joystick = SDL_JoystickOpen(0);
 #endif
 
 /* XXX: currently require non-callback-based audio from SDL >= 2.0.4 */
@@ -129,7 +123,7 @@ void mudclient_start_application(mudclient *mud, char *title) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 #elif defined(OPENGL15)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
