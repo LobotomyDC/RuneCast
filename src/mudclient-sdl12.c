fb420e1224f6fedcf5686b5205266a24bcd1bafc
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2025-06-15 18:05:54 -0400

    Update mudclient-sdl12.c

--- src/mudclient-sdl12.c
+++ src/mudclient-sdl12.c
@@ -2,15 +2,22 @@
 #include <SDL/SDL.h>
 #include <ctype.h>
 #include <kos/dbglog.h>
+#include <SDL/SDL_dreamcast.h>
 
-// Keyboard Buffer
-#define DREAMCAST_KEYBOARD_BUFFER_SIZE 64
+static SDL_Joystick *g_joy;
 
-// D-Pad movement speed
-#define DPAD_SPEED 5
+#define JOYSTICK_MOUSE_SPEED   2.5f
+#define DPAD_MOUSE_SPEED 8
+#define JOYSTICK_DEADZONE      100
 
-void append_to_keyboard_buffer(char c);
-void process_backspace(void);
+/*static int mousemotion_count = 0;
+static uint32_t mousemotion_last_print = 0;*/
+
+void draw_cursor(SDL_Surface *screen, int x, int y) { //making a new cursor because we're trying to use a hwsurface and SDL's cursor is sw-only
+    SDL_Rect rect = { x, y, 8, 8 };
+    Uint32 white = SDL_MapRGB(screen->format, 255, 255, 255);
+    SDL_FillRect(screen, &rect, white);
+}
 
 void mudclient_poll_events(mudclient *mud) {
     SDL_Event event;
@@ -24,17 +31,13 @@ void mudclient_poll_events(mudclient *mud) {
                 break;
 
             case SDL_KEYDOWN: {
-                char char_code = -1;
-                int  code      = -1;
-                get_sdl_keycodes(&event.key.keysym, &char_code, &code);
+                SDLKey sym = event.key.keysym.sym;
+                char cc = -1;
+                int code = -1;
+                get_sdl_keycodes(&event.key.keysym, &cc, &code);
+                //printf("SDL sym=%d  →  mapped code=%d\n", (int)sym, code);
                 if (code != -1) {
-//                    dbglog(DBG_INFO, "Key pressed: %d (%c)\n", code, char_code);
-                    if (char_code != -1 && isprint(char_code)) {
-                        append_to_keyboard_buffer(char_code);
-                    } else if (code == K_BACKSPACE) {
-                        process_backspace();
-                    }
-                    mudclient_key_pressed(mud, code, char_code);
+                    mudclient_key_pressed(mud, code, cc);
                 }
             } break;
 
@@ -43,34 +46,57 @@ void mudclient_poll_events(mudclient *mud) {
                 int  code      = -1;
                 get_sdl_keycodes(&event.key.keysym, &char_code, &code);
                 if (code != -1) {
-//                    dbglog(DBG_INFO, "Key released: %d\n", code);
                     mudclient_key_released(mud, code);
                 }
             } break;
 
+            case SDL_MOUSEMOTION: {
+/*                mousemotion_count++;
+                uint32_t now = SDL_GetTicks();
+                if (now - mousemotion_last_print > 1000) {
+                    printf("SDL_MOUSEMOTION events: %d per second\n", mousemotion_count);
+                    mousemotion_count = 0;
+                    mousemotion_last_print = now;
+                }
+
+                printf("SDL_MOUSEMOTION: x=%d y=%d xrel=%d yrel=%d\n",
+                    event.motion.x, event.motion.y, event.motion.xrel, event.motion.yrel);*/
+
+                mud->mouse_x = event.motion.x;
+                mud->mouse_y = event.motion.y;
+                // Clamp
+/*                if (mud->mouse_x < 0) mud->mouse_x = 0;
+                if (mud->mouse_x >= MUD_WIDTH) mud->mouse_x = MUD_WIDTH - 1;
+                if (mud->mouse_y < 0) mud->mouse_y = 0;
+                if (mud->mouse_y >= MUD_HEIGHT) mud->mouse_y = MUD_HEIGHT - 1;
+                mudclient_mouse_moved(mud, mud->mouse_x, mud->mouse_y);*/
+                break;
+            }
+
             case SDL_MOUSEBUTTONDOWN:
-                switch (event.button.button) {
-                  case 0:  // wheel up
-                    mud->mouse_scroll_delta += +1;
-                    break;
-
-                  case 4:  // wheel down
-                    mud->mouse_scroll_delta += -1;
-                    break;
-
-                  default: // mouse button click
+                printf("MOUSEBUTTONDOWN: button=%d x=%d y=%d\n", event.button.button, event.button.x, event.button.y);
+                if (event.button.button <= 2) { // 0=left, 1=right, 2=middle
                     mudclient_mouse_pressed(
                         mud,
                         event.button.x,
                         event.button.y,
                         event.button.button
                     );
+                }
+                switch (event.button.button) {
+                  case 4:  // wheel up
+                    mud->mouse_scroll_delta += +1;
+                    break;
+
+                  case 5:  // wheel down
+                    mud->mouse_scroll_delta += -1;
                     break;
                 }
                  break;
 
             case SDL_MOUSEBUTTONUP:
-                if (event.button.button >= 1 && event.button.button <= 3) {
+                    printf("MOUSEBUTTONUP: button=%d x=%d y=%d\n", event.button.button, event.button.x, event.button.y);
+                if (event.button.button <= 2) { // 0=left, 1=right, 2=middle
                     mudclient_mouse_released(
                         mud,
                         event.button.x,
@@ -80,58 +106,117 @@ void mudclient_poll_events(mudclient *mud) {
                 }
                 break;
 
-/* FIXME            case SDL_JOYAXISMOTION:
-                if (event.jaxis.axis == 0) {
-                    total_dx += (int)((event.jaxis.value / 32767.0f) * DPAD_SPEED);
-                } else if (event.jaxis.axis == 1) {
-                    total_dy += (int)((event.jaxis.value / 32767.0f) * DPAD_SPEED);
+            //D-Pad
+            case SDL_JOYHATMOTION:
+/*                if (event.jhat.hat == 0) { //D-PAD buttons are Mouse Cartesian Movement
+                    int moved = 0;
+                    if (event.jhat.value & SDL_HAT_LEFT) {
+                        mud->mouse_x -= DPAD_MOUSE_SPEED;
+                        moved = 1;
+                    }
+                    if (event.jhat.value & SDL_HAT_RIGHT) {
+                        mud->mouse_x += DPAD_MOUSE_SPEED;
+                        moved = 1;
+                    }
+                    if (event.jhat.value & SDL_HAT_UP) {
+                        mud->mouse_y -= DPAD_MOUSE_SPEED;
+                        moved = 1;
+                    }
+                    if (event.jhat.value & SDL_HAT_DOWN) {
+                        mud->mouse_y += DPAD_MOUSE_SPEED;
+                        moved = 1;
+                    }
+
+                    if (moved) {
+                        if (mud->mouse_x < 0) mud->mouse_x = 0;
+                        if (mud->mouse_x >= MUD_WIDTH) mud->mouse_x = MUD_WIDTH - 1;
+                        if (mud->mouse_y < 0) mud->mouse_y = 0;
+                        if (mud->mouse_y >= MUD_HEIGHT) mud->mouse_y = MUD_HEIGHT - 1;
+                        mudclient_mouse_moved(mud, mud->mouse_x, mud->mouse_y);
+                        SDL_WarpMouse(mud->mouse_x, mud->mouse_y);
+                    }
                 }
                 break;*/
 
-            case SDL_JOYBUTTONDOWN:
-                mudclient_mouse_pressed(
-                    mud,
-                    mud->mouse_x,
-                    mud->mouse_y,
-                    event.jbutton.button == 0 ? 1 : 3
-                );
+                if (event.jhat.hat == 0) { //D-PAD buttons are Arrow Keys
+                    // left/right on d‐pad → rotate
+                    if (event.jhat.value & SDL_HAT_LEFT)  mudclient_key_pressed(mud, K_LEFT,  0);
+                    else                                   mudclient_key_released(mud, K_LEFT);
+                    if (event.jhat.value & SDL_HAT_RIGHT) mudclient_key_pressed(mud, K_RIGHT, 0);
+                    else                                   mudclient_key_released(mud, K_RIGHT);
+                    // up/down on d‐pad → zoom
+                    if (event.jhat.value & SDL_HAT_UP)    mud->mouse_scroll_delta = +1;
+                    else if (event.jhat.value & SDL_HAT_DOWN) mud->mouse_scroll_delta = -1;
+                }
                 break;
+
+              //Let's get some face buttons in there, too:
+            case SDL_JOYBUTTONDOWN:
+              switch (event.jbutton.button) {
+                case 2:  // “A” → left click
+                  mudclient_mouse_pressed(mud, mud->mouse_x, mud->mouse_y, 0);
+                  break;
+                case 1:  // “B” → right click
+                  mudclient_mouse_pressed(mud, mud->mouse_x, mud->mouse_y, 1);
+                  break;
+                case 6:  // “X” → middle click
+                case 5:  // “Y” → middle click
+                  mudclient_mouse_pressed(mud, mud->mouse_x, mud->mouse_y, 2);
+                  break;
+              }
+              break;
 
             case SDL_JOYBUTTONUP:
-                mudclient_mouse_released(
-                    mud,
-                    mud->mouse_x,
-                    mud->mouse_y,
-                    event.jbutton.button == 0 ? 1 : 3
-                );
-                break;
+              switch (event.jbutton.button) {
+                case 2:
+                  mudclient_mouse_released(mud, mud->mouse_x, mud->mouse_y, 0);
+                  break;
+                case 1:
+                  mudclient_mouse_released(mud, mud->mouse_x, mud->mouse_y, 1);
+                  break;
+                case 6: case 5:
+                  mudclient_mouse_released(mud, mud->mouse_x, mud->mouse_y, 2);
+                  break;
+              }
+          }
+      }
+    if (g_joy) {
+        SDL_JoystickUpdate();
 
-            default:
-                break;
+        // left stick controls mouse cursor
+        Sint16 lx = SDL_JoystickGetAxis(g_joy, 0);
+        Sint16 ly = SDL_JoystickGetAxis(g_joy, 1);
+        if (abs(lx) > JOYSTICK_DEADZONE || abs(ly) > JOYSTICK_DEADZONE) {
+            float fx = lx / 32767.0f;
+            float fy = ly / 32767.0f;
+            mud->mouse_x += (int)(fx * JOYSTICK_MOUSE_SPEED);
+            mud->mouse_y -= (int)(fy * JOYSTICK_MOUSE_SPEED);
+
+
+            // clamp
+/*            if (mud->mouse_x < 0) mud->mouse_x = 0;
+            if (mud->mouse_x >= MUD_WIDTH)  mud->mouse_x = MUD_WIDTH - 1;
+            if (mud->mouse_y < 0) mud->mouse_y = 0;
+            if (mud->mouse_y >= MUD_HEIGHT) mud->mouse_y = MUD_HEIGHT - 1;
+
+            mudclient_mouse_moved(mud, mud->mouse_x, mud->mouse_y);*/
         }
-    }
-}
 
-char dreamcast_keyboard_buffer[DREAMCAST_KEYBOARD_BUFFER_SIZE];
-int keyboard_buffer_pos = 0;
+        // vertical right stick: axis 2 & 3 → camera/mouse-wheel emulation
+        Sint16 rx = SDL_JoystickGetAxis(g_joy, 2);
+        Sint16 ry = SDL_JoystickGetAxis(g_joy, 3);
 
-void reset_keyboard_buffer() {
-    memset(dreamcast_keyboard_buffer, 0, DREAMCAST_KEYBOARD_BUFFER_SIZE);
-    keyboard_buffer_pos = 0;
-}
+        // horizontal right stick → map to “left arrow” / “right arrow” presses
+        if      (rx < -JOYSTICK_DEADZONE) mudclient_key_pressed(mud, K_LEFT,  0);
+        else if (rx >  JOYSTICK_DEADZONE) mudclient_key_pressed(mud, K_RIGHT, 0);
+        else {
+            mudclient_key_released(mud, K_LEFT);
+            mudclient_key_released(mud, K_RIGHT);
+        }
 
-void append_to_keyboard_buffer(char c) {
-    if (keyboard_buffer_pos < DREAMCAST_KEYBOARD_BUFFER_SIZE - 1) {
-        dreamcast_keyboard_buffer[keyboard_buffer_pos++] = c;
-        dreamcast_keyboard_buffer[keyboard_buffer_pos] = '\0';
-    } else {
-        dbglog(DBG_WARNING, "Keyboard buffer full, cannot append character: %c\n", c);
-    }
-}
-
-void process_backspace() {
-    if (keyboard_buffer_pos > 0) {
-        dreamcast_keyboard_buffer[--keyboard_buffer_pos] = '\0';
+        // vertical right stick → mouse wheel
+        if      (ry < -JOYSTICK_DEADZONE) mud->mouse_scroll_delta = +1;
+        else if (ry >  JOYSTICK_DEADZONE) mud->mouse_scroll_delta = -1;
     }
 }
 
@@ -147,20 +232,22 @@ void mudclient_start_application(mudclient *mud, char *title) {
 
 
     if (SDL_NumJoysticks() > 0) {
-        SDL_JoystickOpen(0);
-        SDL_JoystickEventState(SDL_ENABLE);      // turn on stick events
+        g_joy = SDL_JoystickOpen(0);
+        SDL_JoystickEventState(SDL_ENABLE);
+    } else {
+        g_joy = NULL;
     }
-
+   
 //    SDL_DC_EmulateMouse(SDL_TRUE);
-    SDL_ShowCursor(SDL_ENABLE);
-    SDL_WM_GrabInput(SDL_GRAB_ON);
+    SDL_ShowCursor(SDL_DISABLE);
     SDL_EnableUNICODE(1);
+    SDL_EnableKeyRepeat(200, 30); //TODO: Play with this
     SDL_WM_SetCaption(title, NULL);
 
 #ifdef RENDER_SW
     mud->screen = SDL_SetVideoMode(
         mud->game_width, mud->game_height, 16,
-        SDL_SWSURFACE | SDL_FULLSCREEN
+        SDL_HWSURFACE | SDL_FULLSCREEN | SDL_DOUBLEBUF
     );
 #elif defined(RENDER_GL)
     mud->screen = SDL_SetVideoMode(
@@ -168,8 +255,8 @@ void mudclient_start_application(mudclient *mud, char *title) {
         SDL_OPENGL | SDL_FULLSCREEN
     );
 #endif
-mud->mouse_x = MUD_WIDTH  / 2;
-mud->mouse_y = MUD_HEIGHT / 2;
+mud->mouse_x = MUD_WIDTH  / 1;
+mud->mouse_y = MUD_HEIGHT / 1;
 }
 
 #ifdef SDL12
@@ -186,26 +273,29 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
         case SDLK_RETURN:    *code=K_ENTER;     *char_code='\r'; return;
         case SDLK_ESCAPE:    *code=K_ESCAPE;    /*no char*/      return;
         case SDLK_SPACE:     *code=SDLK_SPACE;  *char_code=' ';  return;
-        
-        case SDLK_LEFT: case SDLK_RIGHT: case SDLK_UP: case SDLK_DOWN:
-        case SDLK_F1: case SDLK_F2: case SDLK_F3: case SDLK_F4: case SDLK_F5:
-        case SDLK_F6: case SDLK_F7: case SDLK_F8: case SDLK_F9:
-        case SDLK_F10: case SDLK_F11: case SDLK_F12:
-            *code = keysym->sym;
-            return;
-
-        default:
-            break;
+        case SDLK_LEFT:      *code = K_LEFT;    *char_code= -1;  return;
+        case SDLK_RIGHT:     *code = K_RIGHT;   *char_code= -1;  return;
+        default: break;
     }
 
-        if ((keysym->sym >= SDLK_a && keysym->sym <= SDLK_z) ||
-            (keysym->sym >= SDLK_0 && keysym->sym <= SDLK_9)) {
+    switch (keysym->sym) {
+        case SDLK_UP:    *code = K_UP;    return;
+        case SDLK_DOWN:  *code = K_DOWN;  return;
+
+        case SDLK_F1:  case SDLK_F2:  case SDLK_F3:  case SDLK_F4:
+        case SDLK_F5:  case SDLK_F6:  case SDLK_F7:  case SDLK_F8:
+        case SDLK_F9:  case SDLK_F10: case SDLK_F11: case SDLK_F12:
+          *code = keysym->sym;
+          return;
+    }
+
+    if ((keysym->sym >= SDLK_a && keysym->sym <= SDLK_z) ||
+        (keysym->sym >= SDLK_0 && keysym->sym <= SDLK_9)) {
 
         if (keysym->sym >= SDLK_a) {
             *char_code = (keysym->mod & KMOD_SHIFT)
                 ? toupper(keysym->sym)
                 : tolower(keysym->sym);
-
         } else {
             static const char shifted_map[10] = {
                 ')','!','@','#','$','%','^','&','*','('
@@ -220,6 +310,7 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
         return;
     }
 
+    //Punctuation keys. TODO: Figure out why BACKQUOTE/TILDE doesn't work. It looks like everything is set up correctly?
     switch (keysym->sym) {
       case SDLK_COMMA:        *char_code = (keysym->mod & KMOD_SHIFT) ? '<' : ',';  *code = SDLK_COMMA;        return;
       case SDLK_PERIOD:       *char_code = (keysym->mod & KMOD_SHIFT) ? '>' : '.';  *code = SDLK_PERIOD;       return;
@@ -230,12 +321,13 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
       case SDLK_RIGHTBRACKET: *char_code = (keysym->mod & KMOD_SHIFT) ? '}' : ']';  *code = SDLK_RIGHTBRACKET; return;
       case SDLK_BACKSLASH:    *char_code = (keysym->mod & KMOD_SHIFT) ? '|' : '\\'; *code = SDLK_BACKSLASH;    return;
       case SDLK_MINUS:        *char_code = (keysym->mod & KMOD_SHIFT) ? '_' : '-';  *code = SDLK_MINUS;        return;
-      case SDLK_EQUALS:       *char_code = (keysym->mod & KMOD_SHIFT) ? '+' : '=';  *code = SDLK_EQUALS;       return;
+      case SDLK_PLUS:         *char_code = (keysym->mod & KMOD_SHIFT) ? '+' : '=';  *code = SDLK_PLUS;         return;
       case SDLK_BACKQUOTE:    *char_code = (keysym->mod & KMOD_SHIFT) ? '~' : '`';  *code = SDLK_BACKQUOTE;    return;
       default:
           break;
     }
 
+    //Numpad keys
     if (keysym->sym >= SDLK_KP0 && keysym->sym <= SDLK_KP9) {
         int idx = keysym->sym - SDLK_KP0;
         *char_code = (char)('0' + idx);
