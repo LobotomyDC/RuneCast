6e1b103b93523a39a4e9d1194a3471ba83c601d3
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2025-06-19 20:54:58 -0400

    Update vertex-buffer.c
    Reverting change

--- src/gl/vertex-buffer.c
+++ src/gl/vertex-buffer.c
@@ -1,32 +1,25 @@
 #include "vertex-buffer.h"
-#include "../game-model.h"  // for sizeof(gl_model_vertex)
 
 static gl_vertex_buffer *gl_bound_vertex_buffer = NULL;
 
+// TODO vertex_length = vertex_size
 void vertex_buffer_gl_new(gl_vertex_buffer *vertex_buffer, int vertex_length,
                           int vbo_length, int ebo_length) {
     vertex_buffer->vertex_length = vertex_length;
 
     if (vertex_buffer->attribute_index != 0) {
         vertex_buffer->attribute_index = 0;
+
         vertex_buffer_gl_destroy(vertex_buffer);
     }
 
-#ifdef DREAMCAST
-    vertex_buffer->vbo = malloc(vbo_length * sizeof(gl_model_vertex));
-    vertex_buffer->ebo = malloc(ebo_length * sizeof(GLuint));
-#elif defined(RENDER_GL)
+    /*printf("new vertex buffer %d * %d: %d\n", vbo_length, vertex_length,
+           vbo_length * vertex_length);*/
+
+#ifdef RENDER_GL
     glGenVertexArrays(1, &vertex_buffer->vao);
     glGenBuffers(1, &vertex_buffer->vbo);
     glGenBuffers(1, &vertex_buffer->ebo);
-
-    vertex_buffer_gl_bind(vertex_buffer);
-
-    glBufferData(GL_ARRAY_BUFFER, vbo_length * vertex_length, NULL,
-                 GL_DYNAMIC_DRAW);
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ebo_length * sizeof(GLuint), NULL,
-                 GL_DYNAMIC_DRAW);
 #elif defined(RENDER_3DS_GL)
     vertex_buffer->vbo = linearAlloc(vbo_length * vertex_length);
     vertex_buffer->ebo = linearAlloc(ebo_length * sizeof(uint16_t));
@@ -39,6 +32,16 @@ void vertex_buffer_gl_new(gl_vertex_buffer *vertex_buffer, int vertex_length,
     AttrInfo_Init(&vertex_buffer->attr_info);
     BufInfo_Init(&vertex_buffer->buf_info);
 #endif
+
+#ifdef RENDER_GL
+    vertex_buffer_gl_bind(vertex_buffer);
+
+    glBufferData(GL_ARRAY_BUFFER, vbo_length * vertex_length, NULL,
+                 GL_DYNAMIC_DRAW);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ebo_length * sizeof(GLuint), NULL,
+                 GL_DYNAMIC_DRAW);
+#endif
 }
 
 void vertex_buffer_gl_bind(gl_vertex_buffer *vertex_buffer) {
@@ -47,11 +50,9 @@ void vertex_buffer_gl_bind(gl_vertex_buffer *vertex_buffer) {
     }
 
 #ifdef RENDER_GL
-#ifndef GLDC_GL
     glBindVertexArray(vertex_buffer->vao);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer->vbo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffer->ebo);
-#endif
 #elif defined(RENDER_3DS_GL)
     C3D_SetAttrInfo(&vertex_buffer->attr_info);
     C3D_SetBufInfo(&vertex_buffer->buf_info);
@@ -64,18 +65,20 @@ void vertex_buffer_gl_add_attribute(gl_vertex_buffer *vertex_buffer,
                                     int *attribute_offset,
                                     int attribute_length) {
 #ifdef RENDER_GL
-#ifndef DREAMCAST
 #ifdef OPENGL15
     glVertexAttribPointerARB(vertex_buffer->attribute_index, attribute_length,
                              GL_FLOAT, GL_FALSE, vertex_buffer->vertex_length,
                              (void *)((*attribute_offset) * sizeof(GLfloat)));
-    glEnableVertexAttribArrayARB(vertex_buffer->attribute_index);
 #else
     glVertexAttribPointer(vertex_buffer->attribute_index, attribute_length,
                           GL_FLOAT, GL_FALSE, vertex_buffer->vertex_length,
                           (void *)((*attribute_offset) * sizeof(GLfloat)));
-    glEnableVertexAttribArray(vertex_buffer->attribute_index);
 #endif
+
+#ifdef OPENGL15
+    glEnableVertexAttribArrayARB(vertex_buffer->attribute_index);
+#else
+    glEnableVertexAttribArray(vertex_buffer->attribute_index);
 #endif
 #elif defined(RENDER_3DS_GL)
     AttrInfo_AddLoader(&vertex_buffer->attr_info,
@@ -88,19 +91,14 @@ void vertex_buffer_gl_add_attribute(gl_vertex_buffer *vertex_buffer,
 }
 
 void vertex_buffer_gl_destroy(gl_vertex_buffer *vertex_buffer) {
-    if (!vertex_buffer) return;
+    if (vertex_buffer == NULL) {
+        return;
+    }
 
-#ifdef DREAMCAST
-    free(vertex_buffer->vbo);
-    free(vertex_buffer->ebo);
-    vertex_buffer->vbo = NULL;
-    vertex_buffer->ebo = NULL;
-#elif defined(RENDER_GL)
-#ifndef DREAMCAST
+#ifdef RENDER_GL
     glDeleteVertexArrays(1, &vertex_buffer->vao);
     glDeleteBuffers(1, &vertex_buffer->vbo);
     glDeleteBuffers(1, &vertex_buffer->ebo);
-#endif
 #elif defined(RENDER_3DS_GL)
     linearFree(vertex_buffer->vbo);
     vertex_buffer->vbo = NULL;
