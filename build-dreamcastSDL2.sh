b9557c035a671f0b09d9d84a9af95859aab1ceed
Lobotomy <16826528+LobotomyDC@users.noreply.github.com>
2026-06-15 00:30:40 +0000

    Removing detritus

--- build-dreamcastSDL2.sh
+++
@@ -1,7 +0,0 @@
-source /opt/toolchains/dc/kos/environ.sh
-
-make -f Makefile.dreamcast OLDSDL=1 clean && make -f Makefile.dreamcast OLDSDL=1
-
-# Below is optional to build .cdi. Delete "-N" if you want padding, for whatever reason that may be
-
-#./mkdcdisc -e '[ELF LOCATION]' -d '[ASSET DIRECTORY]' -o RuneScape.cdi -N
